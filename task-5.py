046be4cc8699a2313bf012d99644402a41a35021
Abhiram Manoj Ganta <abhirammanojganta@gmail.com>
2026-07-18 19:06:44 +0530

    Create task-5.py

---
+++ task-5.py
@@ -0,0 +1,44 @@
+contacts = {}
+
+while True:
+    print("\n1. Add Contact")
+    print("2. View Contacts")
+    print("3. Search Contact")
+    print("4. Delete Contact")
+    print("5. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        name = input("Enter name: ")
+        number = input("Enter phone number: ")
+
+        contacts[name] = number
+        print("Contact added")
+
+    elif choice == 2:
+        print(contacts)
+
+    elif choice == 3:
+        name = input("Enter name to search: ")
+
+        if name in contacts:
+            print("Phone Number:", contacts[name])
+        else:
+            print("Contact not found")
+
+    elif choice == 4:
+        name = input("Enter name to delete: ")
+
+        if name in contacts:
+            del contacts[name]
+            print("Contact deleted")
+        else:
+            print("Contact not found")
+
+    elif choice == 5:
+        print("Exit")
+        break
+
+    else:
+        print("Invalid choice")
