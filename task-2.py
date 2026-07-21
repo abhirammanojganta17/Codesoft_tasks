92309b8c44cae59e4948de0b84c1b693fd4cd9c9
Abhiram Manoj Ganta <abhirammanojganta@gmail.com>
2026-07-21 12:16:42 +0000

    enhanced

--- task-2.py
+++ task-2.py
@@ -1,16 +1,31 @@
 print("---------Calculator----------")
+
 num1=float(input("Enter the first number:"))
+
 num2=float(input("Enter the second number:"))
+
 op=input('Enter Operation(+,-,*,/,**):')
+
 if op =="+":
+
     print("Result Of numbers =",num1+num2)
+
 elif op =="-":
+
     print("Result of numbers =",num1-num2)
+
 elif op =="*":
+
     print("Result of numbers =",num1*num2)
+
 elif op =="/":
+
     print("Result of numbers =",num1/num2)
+
 elif op =="**":
+
     print("Result of numbers =",num1**num2)
+
 else:
+
     print("invlid input!")
